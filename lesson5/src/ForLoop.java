42fbacccf9ce52cb2b49007dc40ff9866ce6860e
envisie <34041176+envisie@users.noreply.github.com>
2024-02-05 13:01:57 +0200

    proceed lesson5

---
+++ lesson5/src/ForLoop.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i=10;i>0;i-=5){
+            System.out.println(i);
+        }
+    }
+}
