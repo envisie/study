29f2b0f3f4d167a890f81a17086a20ebd717398a
envisie <34041176+envisie@users.noreply.github.com>
2024-02-05 12:01:38 +0200

    proceed lesson2

---
+++ lesson2/src/Variables.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Variables {
+    public static void main(String[] args) {
+        int myInt; // задекларували, виділили місце в пам`яті для неї
+        myInt = 557;
+
+        short myShort = 3266;
+        long myLong = 26246246;
+        double myDouble = 235.35;
+        float myFloat = 2362.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; // -128 127
+        System.out.println(myInt);
+    }
+}
