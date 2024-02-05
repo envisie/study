fcfd16d2b06e989ceb68b98e30c7e7df60a9b43d
envisie <34041176+envisie@users.noreply.github.com>
2024-02-05 13:16:25 +0200

    proceed lesson6

---
+++ lesson6/src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+        if(myInt<10){
+            System.out.println("<10");
+        } else if (myInt<15) {
+            System.out.println("<15");
+        } else {
+            System.out.println("решта випадків");
+        }
+    }
+}
