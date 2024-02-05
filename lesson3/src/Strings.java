0dc32c3574c142ae836983f80044cfda64f6da54
envisie <34041176+envisie@users.noreply.github.com>
2024-02-05 12:22:07 +0200

    proceed lesson3

---
+++ lesson3/src/Strings.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5; // не застосовується
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello" + " " + "learner");
+        System.out.println("My number is " + s);
+    }
+}
