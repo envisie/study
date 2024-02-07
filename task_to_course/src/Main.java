4dd3cd1bdce468cc6bde5ccefbdbb748d910c381
envisie <34041176+envisie@users.noreply.github.com>
2024-02-07 17:42:51 +0200

    possible solution

---
+++ task_to_course/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(resolveMe(new int[]{1, 2, 3, 4}, 4)); // output: -1
+        System.out.println(resolveMe(new int[]{1, 0, 3, 0}, 4)); // output: 2
+        System.out.println(resolveMe(new int[]{1, 2, 0, 4}, 4)); // output: 3
+        System.out.println(resolveMe(new int[]{1, 2, 3, 0}, 4)); // output: -1
+    }
+
+    public static int resolveMe(int[] arrayNumbers, int arraySize){
+        for(int i=0;i<arraySize-1;i++){
+            if(arrayNumbers[i]==0){
+                try{
+                    if(arrayNumbers[i+2]==0){
+                        return i+1;
+                    }
+                } catch(Exception e){
+                    return i+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
